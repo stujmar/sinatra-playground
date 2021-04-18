dc7761eac7ff6e0e37c688e287b57caec6333aa7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-17 20:16:01 -0500

    [sm] changed route based on path variable

--- myapp.rb
+++ myapp.rb
@@ -6,6 +6,12 @@ get '/' do
     'Hello world!'
 end
 
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  "Hello #{params['name']}!"
+end
+
 post '/post' do
   # create something#
   end
