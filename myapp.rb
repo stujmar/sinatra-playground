4b105c2b41cc34dff3835fdb55df6c30e76b91b9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-04 20:18:14 -0500

    [sm] tested a splat route, pretty cool

--- myapp.rb
+++ myapp.rb
@@ -12,6 +12,18 @@ get '/hello/:name' do
   "Hello #{params['name']}!"
 end
 
+get '/bye/:name' do |n|
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  # n stores params['name']
+  "Buh bye #{n}!"
+end
+
+get '/say/*/to/*' do
+  # matches /say/hello/to/world
+  params['splat']
+end
+
 post '/post' do
   # create something#
   end
