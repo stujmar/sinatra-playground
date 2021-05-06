8f52a69ba7c1359bf3bf8b2c55d0119b62b0a599
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-05 21:07:03 -0500

    [sm] added custom route matchers

--- return.rb
+++ return.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader" if development?
 
+# Return Values
 class Stream
     def each
         100.times { |i| yield "#{i}\n" }
@@ -9,3 +10,32 @@ class Stream
 end
 
 get('/') { Stream.new }
+
+# Custom Route Matchers
+
+class AllButPattern
+    Match = Struct.new(:captures)
+
+    def initialize(except)
+        @exceopt = except
+        @captures = Match.new([])
+    end
+
+    def match(str)
+        @capture unless @except === str
+    end
+end
+
+def all_but(pattern)
+    AllButPattern.new(pattern)
+end
+get all_but("/index") do
+    # ...
+end
+
+# or 
+
+get // do
+    pass if request.path_info == "/index"
+    # ...
+end
